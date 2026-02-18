b7bd4bdafef578befdd90e4dc9b8ae8097d466f7
DenilJosephSunu <deniljosephsunu@gmail.com>
2026-02-18 12:12:53 +0530

    Initial commit - Django Polls App

---
+++ polls/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Question, Choice
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text', 'description']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
---
+++ polls/models.py
@@ -0,0 +1,43 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+from django.contrib import admin
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.question_text
+    
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
+    def total_votes(self):
+        return sum(choice.votes for choice in self.choice_set.all())
+    
+    is_active = models.BooleanField(default=True)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+    
+    def percentage(self):
+        total = self.question.total_votes()
+        if total == 0:
+            return 0
+        return round((self.votes / total) * 100, 2)
+    
+    
